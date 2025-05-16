98b1e4c12e61b693085d4e6d00ea085e40b6903a
DExavitM <210070614+NathansSoftwareDevelopment@users.noreply.github.com>
2025-05-15 20:20:07 -0500

    Added Pokemon_Class
    Added "Pokemon_Class" File with Class "Pokemon."
    
    "Pokemon" Class currently has Public variables, Constructor outlines, and a Destructor outline.

---
+++ Pokemon_Class.cpp
@@ -0,0 +1,70 @@
+#include <string>
+
+class Pokemon {
+    public:
+        std::string pokemonSpecies;
+        std::string pokemonName;
+
+        int pokemonLevel;
+        int pokemonExperience;
+
+        std::string pokemonItem;
+
+        std::string pokemonAbility;
+
+        std::string pokemonType1;
+        std::string pokemonType2;
+
+        std::string pokemonMove1;
+        std::string pokemonMove2;
+        std::string pokemonMove3;
+        std::string pokemonMove4;
+        
+        std::string pokemonCondition;
+
+        int speciesHitPoints;
+        int speciesAttack;
+        int speciesDefense;
+        int speciesSpecialAttack;
+        int speciesSpecialDefense;
+        int speciesSpeed;
+        
+        int pokemonHitPointsIndividualValue;
+        int pokemonAttackIndividualValue;
+        int pokemonDefenseIndividualValue;
+        int pokemonSpecialAttackIndividualValue;
+        int pokemonSpecialDefenseIndividualValue;
+        int pokemonSpeedIndividualValue;
+
+        int pokemonHitPointsEffortValue;
+        int pokemonAttackEffortValue;
+        int pokemonDefenseEffortValue;
+        int pokemonSpecialAttackEffortValue;
+        int pokemonSpecialDefenseEffortValue;
+        int pokemonSpeedEffortValue;
+
+        int pokemonHitPoints;
+        int pokemonAttack;
+        int pokemonDefense;
+        int pokemonSpecialAttack;
+        int pokemonSpecialDefense;
+        int pokemonSpeed;
+
+        int currentHitPoints;
+        int currentAttack;
+        int currentDefense;
+        int currentSpecialAttack;
+        int currentSpecialDefense;
+        int currentSpeed;
+        
+        Pokemon(std::string pokemonSpecies, std::string pokemonName, std::string pokemonAbility, int speciesStats[6], int pokemonIndividualValues[6]);
+        Pokemon(
+            std::string pokemonSpecies, std::string pokemonName, int pokemonLevel, int pokemonExperience, std::string pokemonAbility,
+            std::string pokemonType1, std::string pokemonType2,
+            std::string pokemonMove1, std::string pokemonMove2, std::string pokemonMove3, std::string pokemonMove4, 
+            std::string pokemonCondition,
+            int speciesStats[6], int pokemonIndividualValues[6], int pokemonEffortValues[6]
+        );
+        ~Pokemon();
+};
+
