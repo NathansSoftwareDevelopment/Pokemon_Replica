f1ebbfc35001f6ad9e53a77e1ddcef331a230cf0
DExavitM <210070614+NathansSoftwareDevelopment@users.noreply.github.com>
2025-05-20 21:30:13 -0500

    Smokescreen Damage
    Fixed "Smokescreen" Move dealing 40 damage.

--- All_Moves.cpp
+++ All_Moves.cpp
@@ -8,7 +8,7 @@ std::map<std::string, Move> moveMap = {
     {"Growl", Move("Growl", "Normal", "Status", 0, 100, 40, emptyMap, 0, false)},
     {"Scratch", Move("Scratch", "Normal", "Physical", 40, 100, 40, emptyMap, 0, false)},
     {"Ember", Move("Ember", "Fire", "Special", 40, 100, 40, {{"Burn", 10}}, 0, false)},
-    {"Smokescreen", Move("Smokescreen", "Normal", "Status", 40, 100, 20, emptyMap, 0, true)},
+    {"Smokescreen", Move("Smokescreen", "Normal", "Status", 0, 100, 20, emptyMap, 0, true)},
     {"Dragon Rage", Move("Scratch", "Normal", "Special", 0, 100, 10, emptyMap, 0, true)},
     {"Scary Face", Move("Scary Face", "Normal", "Status", 40, 100, 10, emptyMap, 0, true)},
     {"Fire Fang", Move("Fire Fang", "Fire", "Physical", 65, 95, 15, {{"Burn", 10}}, 10, false)},
