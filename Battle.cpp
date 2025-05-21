ebc11b737ea3383aae0ed718e775af7f1425d7c8
DExavitM <210070614+NathansSoftwareDevelopment@users.noreply.github.com>
2025-05-20 21:29:28 -0500

    Added Battle
    Added a basic system to allow for pokemon battles ala "Battle" Class in its respective .h and .cpp Files.

---
+++ Battle.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <random>
+#include <algorithm>
+#include "Battle.h"
+#include "Pokemon_Class.h"
+#include "Type.h"
+#include "Move.h"
+
+std::random_device rd;
+std::mt19937 gen(rd());
+
+Move* Battle::battleGetMove(Pokemon* attackingInputPokemon, int battleInputMove) {
+    return *(attackingInputPokemon->pokemonMoves[battleInputMove-1]);
+}
+void Battle::battleDamageCalculation(Pokemon* attackingInputPokemon, Move* battleInputPokemonMove, Pokemon* defendingInputPokemon) {
+    std::uniform_int_distribution<int> distribution(1, 100);
+    int randomNumber = distribution(gen);
+    if (randomNumber > battleInputPokemonMove->moveAccuracy) {
+        return;
+    }
+    double stab;
+    if (battleInputPokemonMove->moveType == attackingInputPokemon->pokemonType1 || battleInputPokemonMove->moveType == attackingInputPokemon->pokemonType2) {
+        stab = 1.5;
+    } else {
+        stab = 1;
+    }
+    double effectiveness = battleInputPokemonMove->moveType->attackingTypeMap.find(defendingInputPokemon->pokemonType1->typeName)->second * battleInputPokemonMove->moveType->attackingTypeMap.find(defendingInputPokemon->pokemonType2->typeName)->second;
+    int rawDamage = ((2*attackingInputPokemon->pokemonLevel/5+2)*battleInputPokemonMove->movePower*attackingInputPokemon->currentAttack/defendingInputPokemon->currentDefense/50+2);
+    int totalDamage = rawDamage * stab * effectiveness;
+    defendingInputPokemon->currentHitPoints = std::max(defendingInputPokemon->currentHitPoints - totalDamage, 0);
+}
+void Battle::battleGetFasterPokemon(Pokemon* inputPokemon1, Pokemon* inputPokemon2) {
+    if (inputPokemon1->currentSpeed > inputPokemon2->currentSpeed) {
+        battleFasterPokemon = inputPokemon1;
+        battleSlowerPokemon = inputPokemon2;
+    } else if (inputPokemon1->currentSpeed < inputPokemon2->currentSpeed) {
+        battleFasterPokemon = inputPokemon2;
+        battleSlowerPokemon = inputPokemon1;
+    } else {
+        std::uniform_int_distribution<int> distribution(1, 2);
+        int randomNumber = distribution(gen);
+        if (randomNumber == 1) {
+            battleFasterPokemon = inputPokemon1;
+            battleSlowerPokemon = inputPokemon2;
+        } else {
+            battleFasterPokemon = inputPokemon2;
+            battleSlowerPokemon = inputPokemon1;
+        }
+    }
+}
+Battle::Battle(Pokemon* inputPokemon1, Pokemon* inputPokemon2) {
+    std::cout << "\n\n\n";
+    battleTurn = 0;
+    while (inputPokemon1->currentHitPoints > 0 && inputPokemon2->currentHitPoints > 0) {
+        battleTurn++;
+        battleGetFasterPokemon(inputPokemon1, inputPokemon2);
+        std::cout << "Turn " << battleTurn << std::endl;
+        std::cout << "First: " << battleFasterPokemon->pokemonName << std::endl;
+        int moveSlot;
+        std::cout << "What Move Slot to Use? ";
+        std::cin >> moveSlot;
+        std::cout << std::endl;
+        Move* battleFasterPokemonMove = battleGetMove(battleFasterPokemon, moveSlot);
+        std::cout << "Second: " << battleSlowerPokemon->pokemonName << std::endl;
+        std::cout << "What Move Slot to Use? ";
+        std::cin >> moveSlot;
+        std::cout << std::endl;
+        Move* battleSlowerPokemonMove = battleGetMove(battleSlowerPokemon, moveSlot);
+        std::cout << battleFasterPokemon->pokemonName << " Used " << battleFasterPokemonMove->moveName << std::endl;
+        if (battleFasterPokemonMove->moveDamageCategory != "Status") {
+            battleDamageCalculation(battleFasterPokemon, battleFasterPokemonMove, battleSlowerPokemon);
+        }
+        if (battleSlowerPokemon->currentHitPoints > 0) {
+            if (battleSlowerPokemonMove->moveDamageCategory != "Status") {
+                std::cout << battleSlowerPokemon->pokemonName << " Used " << battleSlowerPokemonMove->moveName << std::endl;
+                battleDamageCalculation(battleSlowerPokemon, battleSlowerPokemonMove, battleFasterPokemon);
+            }
+        }
+    }
+    std::cout << "Battle Over" << std::endl;
+    std::cout << inputPokemon1->currentHitPoints << std::endl;
+    std::cout << inputPokemon2->currentHitPoints << std::endl;
+}
---
+++ Battle.h
@@ -0,0 +1,20 @@
+#ifndef BATTLE_H
+#define BATTLE_h
+#include "Pokemon_Class.h"
+#include "Move.h"
+
+class Battle {
+    public:
+        int battleTurn;
+        bool battlePlayersTurn;
+        Move* battlePlayer1Move;
+        Move* battlePlayer2Move;
+        Pokemon* battleFasterPokemon;
+        Pokemon* battleSlowerPokemon;
+        void battleDamageCalculation(Pokemon* attackingInputPokemon, Move* battleInputPokemonMove, Pokemon* defendingInputPokemon);
+        Move* battleGetMove(Pokemon* attackingInputPokemon, int battleInputMove);
+        void battleGetFasterPokemon(Pokemon* inputPokemon1, Pokemon* inputPokemon2);
+        Battle(Pokemon* inputPokemon1, Pokemon* inputPokemon2);
+};
+
+#endif
