abf3c0f066727fd8db155426e7ef8ea917702a36
NathansSoftwareDevelopment <nathanhogen2005@gmail.com>
2025-05-31 18:50:33 -0500

    Changed Move.cpp to work with Move.json

--- Move.cpp
+++ Move.cpp
@@ -27,36 +27,31 @@ Move::Move(
 
 
 void from_json(const json& inputJson, Move& inputMove) {
-    std::string typeString = inputJson.at("type").at("name").get<std::string>();
-    typeString[0] = static_cast<char>(std::toupper(typeString[0]));
+    inputMove.name = inputJson.at("name");
+
+    std::string typeString = inputJson.at("type").get<std::string>();
     inputMove.type = &typeMap.find(typeString)->second;
 
-    std::string categoryString = inputJson.at("damage_class").at("name").get<std::string>();
-    categoryString[0] = static_cast<char>(std::toupper(categoryString[0]));
-    inputMove.damageCategory = categoryString;
+    inputMove.damageCategory = inputJson.at("damageCategory").get<std::string>();
 
-    if (inputJson.at("power").is_null()) {
-        inputMove.power = 0;
-    } else {
-        inputMove.power = inputJson.at("power").get<int>();
-    }
+    inputMove.power = inputJson.at("power").get<int>();
 
     inputMove.accuracy = inputJson.at("accuracy").get<int>();
 
-    inputMove.PP = inputJson.at("pp").get<int>();
+    inputMove.PP = inputJson.at("PP").get<int>();
 
-    inputMove.flinchChance = inputJson.at("meta").at("flinch_chance").get<int>();
+    inputMove.flinchChance = inputJson.at("flinchChance").get<int>();
 
-    // inputMove.conditionChances;
+    inputMove.conditionChances;
 
-    // inputMove.userStageChances;
-    // inputMove.opponentStageChances;
+    inputMove.userStageChances;
+    inputMove.opponentStageChances;
 }
 
 std::map<std::string, Move> moveMap;
 
 void generateMoveMap() {
-    std::string filePath = "..\\Raw_Data\\temp_moves.json";
+    std::string filePath = "../Move.json";
     std::ifstream ifs(filePath);
     if (!ifs.is_open()) {
         std::cerr << "Error opening file: " << filePath << std::endl;
@@ -66,7 +61,6 @@ void generateMoveMap() {
 
     for (json::iterator move = movesFile.begin(); move != movesFile.end(); move++) {
         std::string moveName = move.key();
-        moveName[0] = static_cast<char>(std::toupper(moveName[0]));
         json movesJSON = move.value();
 
         moveMap[moveName] = movesJSON.get<Move>();
