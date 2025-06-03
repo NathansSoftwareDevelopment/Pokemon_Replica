b3a78d431cd848dc8a436a37512ae77b2a569384
NathansSoftwareDevelopment <nathanhogen2005@gmail.com>
2025-06-03 18:49:08 -0500

    Added Trainer.cpp (with Constructors)

---
+++ Trainer.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <map>
+#include "Trainer.h"
+#include "Pokemon_Class.h"
+
+int Trainer::_totalTrainers = 0;
+
+Trainer::Trainer(std::string inputName, Pokemon* inputPokemon) {
+    _name = inputName;
+    _pokemon[1] = inputPokemon;
+    _totalTrainers++;
+    _ID = _totalTrainers;
+}
+
+Trainer::Trainer(std::string inputName, std::map<int, Pokemon*> inputPokemon) {
+    _name = inputName;
+    _pokemon = inputPokemon;
+    _totalTrainers++;
+    _ID = _totalTrainers;
+}
