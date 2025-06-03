05f2aadf44ea7f3dce4785383f622d3e3be7038c
NathansSoftwareDevelopment <nathanhogen2005@gmail.com>
2025-06-03 18:11:03 -0500

    Added Trainer.h File

---
+++ Trainer.h
@@ -0,0 +1,23 @@
+#ifndef TRAINER_H
+#define TRAINER_H
+#include <map>
+#include <string>
+#include "Pokemon_Class.h"
+
+class Trainer {
+    private:
+        std::string _name;
+        int _ID;
+        std::map<int, Pokemon*> _pokemon;
+    public:
+        const std::string name() const {return _name;}
+        const int ID() const {return _ID;}
+        const std::map<int, Pokemon*> pokemon() const {return _pokemon;}
+
+        Trainer& pokemon(int inputSlot, Pokemon* inputPokemon) {_pokemon[inputSlot]=inputPokemon; return *this;}
+
+        Trainer(std::string inputName, Pokemon* inputPokemon);
+        Trainer(std::string inputName, std::map<int, Pokemon*> inputPokemon);
+};
+
+#endif
