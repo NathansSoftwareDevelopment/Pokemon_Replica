2f4b10adca0ef5e57ca34197d1955948bf22bd9f
NathansSoftwareDevelopment <nathanhogen2005@gmail.com>
2025-06-05 20:50:44 -0500

    Item is now an Abstract Class
    Basically there is never a time where you just want to create an "Item" (not to be confused with "Items" which will be a subclass) so Item has been changed to an Abstract Class.

--- Item/Item.h
+++ Item/Item.h
@@ -4,16 +4,16 @@
 #include <map>
 
 class Item {
-    private:
+    protected:
         std::string _name;
         bool _holdable;
         std::pair<std::string, int> _pocket;
+
+        Item(std::string inputName, bool inputHoldable, std::string inputPocket);
     public:
         const std::string name() const {return _name;}
         const bool holdable() const {return _holdable;}
         const std::pair<std::string, int> pocket() const {return _pocket;}
-
-        Item(std::string inputName, bool inputHoldable, std::string inputPocket);
 };
 
 extern const std::map<std::string, int> bagLookupMap;
