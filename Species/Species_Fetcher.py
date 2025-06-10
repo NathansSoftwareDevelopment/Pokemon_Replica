3302666c3b5b88abf59c88ef78acffe04437bf50
NathansSoftwareDevelopment <nathanhogen2005@gmail.com>
2025-06-09 21:41:39 -0500

    Fetchers now put JSONs in the correct directories
    Ever since the file structure change the fetchers have been making their JSONs in root. This has been changed so they now put them in the appropriate Directory.

--- Species/Species_Fetcher.py
+++ Species/Species_Fetcher.py
@@ -15,7 +15,7 @@ def main():
         pokemonName = allRawData[0][pokemon][0]
         addToFinal(parsePokeInfo([allRawData[0][pokemonName["name"]][0], allRawData[-1][pokemonName["species"]["name"]][0]]))
         ic(pokemonName["name"])
-    Write(outputDictionary, "Species.json")
+    Write(outputDictionary, r"./Species/Species.json")
 
 
 def getPokemonInfo(*args):
