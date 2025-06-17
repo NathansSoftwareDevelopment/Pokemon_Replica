4eaaf5ddf54cd884681130c9f7cba572ab0fa5f4
NathansSoftwareDevelopment <nathanhogen2005@gmail.com>
2025-06-16 19:44:04 -0500

    Cleaning for conversion to C#
    Over the past few days I have been thinking about how to actually put this game on the screen and decided to use Unity which will require a transfer to C#.
    
    The old C++ code is now in the "cpp-legacy" Branch.

--- Item/Item.cpp
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include <map>
-#include "Item.h"
-
-Item::Item(std::string inputName, bool inputHoldable, std::string inputPocket) {
-    _name = inputName;
-    _holdable = inputHoldable;
-    _pocket = {inputPocket, bagLookupMap.at(inputPocket)};
-}
