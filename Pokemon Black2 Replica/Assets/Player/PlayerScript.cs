8a055df855d66fc3af91099cd095a759152d58e3
NathansSoftwareDevelopment <nathanhogen2005@gmail.com>
2025-06-16 23:16:29 -0500

    Added Types!
    'TypeMap' is now a Static Attribute of 'Type'.

--- Pokemon Black2 Replica/Assets/Player/PlayerScript.cs
+++ Pokemon Black2 Replica/Assets/Player/PlayerScript.cs
@@ -5,6 +5,7 @@ public class PlayerScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        Debug.Log(Type.TypeMap["Steel"].AttackingTypeMap["Fighting"]);
     }
 
     // Update is called once per frame
