f926da9aaab343d7e26f5da0d03ba251b9aaee86
NathansSoftwareDevelopment <nathanhogen2005@gmail.com>
2025-06-17 21:56:58 -0500

    Original Unity Project corrupted?
    So I tried adding an ActionMap to my project and tried basically everything (even reinstalls of the package and unity) and decided it was less work to just delete the fledgling Unity Project and start over than to keep debugging.
    
    Also added primitive player movement and redid the .gitignores.

---
+++ Pokemon Black2 Replica/Assets/Player/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+                  
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private int movementSpeed = 4;
+    private Vector2 movementDirection;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnMovement(InputValue keystroke)
+    {
+        movementDirection = keystroke.Get<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movementDirection);
+    }
+}
