c4378ee2792a937f638f0c0f0854aa253d90dddd
NathansSoftwareDevelopment <nathanhogen2005@gmail.com>
2025-06-18 21:25:01 -0500

    Improved Movement!
    Player can now only move 'movementSpeed' times per second. Currently only works with taps and not holding down, but I'll get there in a bit.

--- Pokemon Black2 Replica/Assets/Player/PlayerMovement.cs
+++ Pokemon Black2 Replica/Assets/Player/PlayerMovement.cs
@@ -3,23 +3,78 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private @PlayerInputs playerInputs;
+    Vector2 movementDirection;
     [SerializeField] private int movementSpeed = 4;
-    private Vector2 movementDirection;
+    private double movementTime = 0;
+    private double movementCooldown = 0;
     private Rigidbody2D rb;
 
     private void Awake()
     {
+        playerInputs = new @PlayerInputs();
+        movementTime = 1.0/movementSpeed;
         rb = GetComponent<Rigidbody2D>();
+
+        playerInputs.Player.Movement.started += OnMovementPerformed;
+        playerInputs.Player.Movement.performed += OnMovementPerformed;
+        playerInputs.Player.Movement.canceled += OnMovementPerformed;
     }
 
-    private void OnMovement(InputValue keystroke)
+    private void OnValidate()
     {
-        movementDirection = keystroke.Get<Vector2>();
-        Debug.Log("Moved");
+        movementTime = 1.0 / movementSpeed;
     }
 
-    private void FixedUpdate()
+    private void OnEnable()
     {
-        rb.MovePosition(rb.position + movementDirection);
+        playerInputs.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInputs.Disable();
+    }
+
+    public void OnMovementPerformed(InputAction.CallbackContext context)
+    {
+        if (context.phase.ToString() == "Performed")
+        {
+            movementDirection = context.ReadValue<Vector2>();
+            Debug.Log($"Movement {context.control.displayName} {movementDirection}");
+        } else
+        {
+            return;
+        }
+
+        if (MovementValid(context))
+        {
+            rb.MovePosition(rb.position + movementDirection);
+            Debug.Log("Valid Movement!");
+        }
+        else
+        {
+            Debug.Log("Invalid Movement.");
+        }
+    }
+
+    private bool MovementValid(InputAction.CallbackContext context)
+    {
+        if (movementCooldown < movementTime)
+        {
+            return false;
+        } else
+        {
+            movementCooldown = 0;
+            return true;
+        }
+    }
+
+    private void Update()
+    {
+        if (movementCooldown < movementTime)
+        {
+            movementCooldown += Time.deltaTime;
+        }
     }
 }
