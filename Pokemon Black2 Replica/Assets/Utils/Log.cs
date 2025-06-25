df60b6094de020b7d37188f7f28427738b8be75f
NathansSoftwareDevelopment <nathanhogen2005@gmail.com>
2025-06-25 14:53:12 -0500

    Declared AutoLogMessage
    Declared an "AutoLogMessage" Method which will determine the log message if one is not provided.

--- Pokemon Black2 Replica/Assets/Utils/Log.cs
+++ Pokemon Black2 Replica/Assets/Utils/Log.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 namespace Utils
@@ -7,7 +8,15 @@ namespace Utils
         public static void Log<T>(this T logObject, string logMessage = "")
         {
             string logObjectInformation = "";
+            logMessage = AutoLogMessage(logObject, logMessage);
             Debug.Log($"{logMessage}\n{logObjectInformation}");
+        }
+
+        private static string AutoLogMessage(object inputObject, string inputMessage)
+        {
+            if (!string.IsNullOrEmpty(inputMessage)) { return inputMessage; }
+
+            throw new System.Exception("ERROR: SHOULD NOT REACH THIS POINT");
         }
     }
 }
